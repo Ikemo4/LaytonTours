beba4237be4965314e85aeca7f320afc4486f32a
Ikemo4 <67703806+Ikemo4@users.noreply.github.com>
2021-03-17 10:04:29 -0600

    Add project files.

---
+++ LaytonTours/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTours.Models
+{
+    public class Appointment
+    {
+        [Key]
+        [Required]
+        public int AppointmentId { get; set; }
+        [Required]
+        public string GroupName { get; set; }
+        [Required]
+        public int GroupSize { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int TimeID { get; set; }
+
+        //GetTime (from timeID)
+    }
+}
---
+++ LaytonTours/Models/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTours.Models
+{
+    public class Time
+    {
+        [Key]
+        public int TimeID { get; set; }
+        //appointmentID
+        public int? AppointmentID { get; set; }
+        public string ScheduledTime { get; set; }
+        public string Date { get; set; }
+        //getAvailableTimes method
+    }
+}
