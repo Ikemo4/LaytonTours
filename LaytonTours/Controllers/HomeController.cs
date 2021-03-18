8444a60c591128a27d43f50277b1c48b0caf39ff
Ikemo4 <67703806+Ikemo4@users.noreply.github.com>
2021-03-18 08:46:12 -0600

    isaac's changes

--- LaytonTours/Controllers/HomeController.cs
+++ LaytonTours/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaytonTours.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,19 +10,69 @@ namespace LaytonTours.Controllers
 {
     public class HomeController : Controller
     {
+        private ITourRepository _repository;
+        private readonly ILogger<HomeController> _logger;
+        private List<Time> times = new List<Time>();
+
+        public HomeController(ILogger<HomeController> logger, ITourRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+
+
+
+            // Used to generate times
+            DateTime currentDate = DateTime.Now;
+            int startTime = 4;
+            int objectsCreated = 0;
+
+            for (int days = 0; days < 7; days++)
+            {
+                for (int hours = 0; hours < 3; hours++)
+                {
+                    Time newTime = new Time();
+                    newTime.Date = currentDate.AddDays(days).ToString("dd/MM/yyyy");
+                    newTime.ScheduledTime = (startTime + hours).ToString() + ":00 PM";
+                    newTime.TimeID = objectsCreated;
+                    objectsCreated++;
+                    times.Add(newTime);
+                }
+            }
+
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Times = times;
             return View();
         }
 
         public IActionResult ViewAppointments()
         {
+            ViewBag.Times = times;
             return View();
         }
 
+        [HttpGet("SignUp")]
         public IActionResult SignUp()
         {
+            ViewBag.Times = times;
+            // Uncomment once code is ready - return View(Time.getAvailableTimes());
             return View();
+        }
+
+        [HttpPost("SignUp")]
+        public IActionResult SignUp(Appointment appointment)
+        {
+            ViewBag.Times = times;
+            if (ModelState.IsValid)
+            {
+
+                return View("ViewAppointments");
+            }
+
+            return View();
+            // Uncomment once code is ready - return View(Time.getAvailableTimes());
         }
     }
 }
--- LaytonTours/Startup.cs
+++ LaytonTours/Startup.cs
@@ -20,7 +20,7 @@ namespace LaytonTours
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
